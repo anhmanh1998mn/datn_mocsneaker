933968d3ad971c18afa9256ddc55ff3504ac2eb2
anhmanh1998mn <anhmanh1998mq@gmail.com>
2020-09-24 16:07:36 +0700

    cvManh_commit

--- app/src/main/java/com/example/manhvan/datn_mocsneaker/MyService/APIService.java
+++ app/src/main/java/com/example/manhvan/datn_mocsneaker/MyService/APIService.java
@@ -1,7 +1,7 @@
 package com.example.manhvan.datn_mocsneaker.MyService;
 
 public class APIService {
-    private static String base_url="http://192.168.52.116:8080/datn_mocsneakerapi/";
+    private static String base_url="http://192.168.42.44/datn_mocsneakerapi/";
     public static Dataservice getService(){
         return APIRetrofitClient.getClient(base_url).create(Dataservice.class);
     }
