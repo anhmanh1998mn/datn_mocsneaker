b9ec22360bb28c6a47d5500c75c6a69a67b716bc
manhvan1998@outlook.com <manhvan1998@outlook.com>
2020-10-15 22:28:34 +0700

    Merge branch 'master' of https://github.com/anhmanh1998mn/datn_mocsneaker
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser

--- app/src/main/java/com/example/manhvan/datn_mocsneaker/View/MainChiTietDonNhap.java
+++ app/src/main/java/com/example/manhvan/datn_mocsneaker/View/MainChiTietDonNhap.java
@@ -3,12 +3,20 @@ package com.example.manhvan.datn_mocsneaker.View;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.manhvan.datn_mocsneaker.R;
 
 public class MainChiTietDonNhap extends AppCompatActivity {
     private ActionBar actionBar;
+    private TextView txtMaDonNhapCT,txtNgayLapCT,txtTongSL,txtTongTienCT;
+    private Spinner spinner;
+    private RecyclerView recyclerViewCT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,18 @@ public class MainChiTietDonNhap extends AppCompatActivity {
         actionBar=getSupportActionBar();
         actionBar.setTitle("Chi tiết đơn nhập hàng");
         actionBar.setDisplayHomeAsUpEnabled(true);
+        initView();
+    }
+
+    private void initView() {
+        txtMaDonNhapCT=findViewById(R.id.txt_maDonNhapCT);
+        txtNgayLapCT=findViewById(R.id.txt_ngayLapCT);
+        spinner=findViewById(R.id.spinner_CT);
+        recyclerViewCT=findViewById(R.id.recycle_viewCT);
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this, LinearLayout.VERTICAL,false);
+        recyclerViewCT.setLayoutManager(linearLayoutManager);
+        txtTongSL=findViewById(R.id.txt_tongSL);
+        txtTongTienCT=findViewById(R.id.txt_tongTienCT);
     }
 
     @Override
