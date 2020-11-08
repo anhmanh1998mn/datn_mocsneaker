b46735d12592195c81993eb58d4cae5b32579132
manhvan1998@outlook.com <manhvan1998@outlook.com>
2020-11-08 12:44:03 +0700

    Merge branch 'master' of https://github.com/anhmanh1998mn/datn_mocsneaker
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser

--- app/src/main/java/com/example/manhvan/datn_mocsneaker/MyService/APIService.java
+++ app/src/main/java/com/example/manhvan/datn_mocsneaker/MyService/APIService.java
@@ -4,7 +4,7 @@ public class APIService {
 //        private static String base_url="http://192.168.42.44/datn_mocsneakerapi/";
     private static String base_url="http://192.168.42.44:8080/datn_mocsneakerapi/";
 //    private static String base_url = "http://192.168.24.10:8080/datn_mocsneakerapi/";
-
+//abc
     public static Dataservice getService() {
         return APIRetrofitClient.getClient(base_url).create(Dataservice.class);
     }
